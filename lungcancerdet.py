42fb222790b1849adfaa3eb119b2431e0baecbf7
Shubhrojit Panda <shubhrojitp05@gmail.com>
2025-04-11 12:58:15 +0530

    initial commit

--- lungcancerdet.py
+++ lungcancerdet.py
@@ -21,3 +21,27 @@ plt.title('Bengin Case')
 plt.imshow(img, label = 'Bengin Case')
 plt.axis()
 plt.show()
+img = cv2.imread(r'C:\Users\Shubhrojit Panda\Desktop\New folder\LungcancerDataSet\Data\test\MalignantCases\Malignant case (464).jpg')
+plt.title('Mailgnant Case')
+plt.imshow(img, label = 'Malignant Case')
+plt.axis()
+plt.show()
+img = cv2.imread(r'C:\Users\Shubhrojit Panda\Desktop\New folder\LungcancerDataSet\Data\test\normal\24.png')
+plt.title('Normal Case')
+plt.imshow(img,label = 'Normal Case')
+plt.axis()
+plt.show()
+print(img.shape)
+dir = r'C:\Users\Shubhrojit Panda\Desktop\New folder\The IQ-OTHNCCD lung cancer dataset'
+img_width= 256
+img_height = 256
+categories = ['Bengin cases, Malignant cases', 'Normal cases']
+img_data=[]
+for cata in categories:
+    folder = os.path.join(dir, cata)
+    label = categories.index(cata)
+    for img in os.listdir(folder):
+        img_path = os.path.join(folder, img)
+        try:
+            img_array = cv2.imread(img_path)
+            img_array = cv2.resize(img_array, (img_height,img_width))
